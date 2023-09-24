95052b9754bc1514ea214a0ee37c1ccb9ac7ad25
Alex8608 <aleshka.vlad@gmail.com>
2023-09-24 17:22:45 +0400

    print all dates and values

--- Lab.py
+++ Lab.py
@@ -1,7 +1,8 @@
 import requests
 
 url="https://www.cbr-xml-daily.ru/archive/2023/09/23/daily_json.js"
-
-response=requests.get(url)
-data=response.json()
-print(data["Date"][:10], data["Valute"]["USD"]["Value"])
+while url!="https://www.cbr-xml-daily.ru/archive/2021/01/01/daily_json.js":
+    response=requests.get(url)
+    data=response.json()
+    print(data["Date"][:10], data["Valute"]["USD"]["Value"])
+    url="https:"+data["PreviousURL"]
