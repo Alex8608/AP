4e7e895530dd9d2fef71f73a613268e23da1e608
Alex8608 <aleshka.vlad@gmail.com>
2023-09-24 18:01:15 +0400

    datalist

--- Lab.py
+++ Lab.py
@@ -1,8 +1,9 @@
 import requests
-
+array=[]
 url="https://www.cbr-xml-daily.ru/archive/2023/09/23/daily_json.js"
 while url!="https://www.cbr-xml-daily.ru/archive/2021/01/01/daily_json.js":
     response=requests.get(url)
     data=response.json()
-    print(data["Date"][:10], data["Valute"]["USD"]["Value"])
+    array.append(f'{data["Date"][:10]},{data["Valute"]["USD"]["Value"]}')
     url="https:"+data["PreviousURL"]
+array.reverse()
