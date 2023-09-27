89e22e5fb30f9427070edaf80a85e72f3521f847
Alex8608 <aleshka.vlad@gmail.com>
2023-09-27 18:00:48 +0400

    Add the possibility of selecting a period

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -1,13 +1,27 @@
 import requests
+
+def date_conrol():
+    while True:
+        date=str(input())
+        try:
+            requests.get(f"https://www.cbr-xml-daily.ru/archive/{date}/daily_json.js").json()["Date"]
+            return f"https://www.cbr-xml-daily.ru/archive/{date}/daily_json.js"
+        except:
+            print("Курс ЦБ РФ на данную дату не установлен\nУкажите другую дату: ")
+
+print("Введите дату начала периода в формате yyyy/mm/dd: ")
+url1=date_conrol()
+print("Введите дату конца периода в формате yyyy/mm/dd: ")
+url2=date_conrol()
 array=[]
-url="https://www.cbr-xml-daily.ru/archive/2023/09/23/daily_json.js"
-while url!="https://www.cbr-xml-daily.ru/archive/2023/02/03/daily_json.js":
-    response=requests.get(url)
+while url2!=url1:
+    response=requests.get(url2)
     data=response.json()
     array.append(f'{data["Date"][:10]},{data["Valute"]["USD"]["Value"]}')
-    url="https:"+data["PreviousURL"]
+    url2="https:"+data["PreviousURL"]
+array.append(f'{data["PreviousDate"][:10]},{data["Valute"]["USD"]["Previous"]}')
 array.reverse()
-file=open("dataset.csv","w")
+file=open("./Lab1/dataset.csv","w")
 s="\n"
 file.write(s.join(array))
 file.close()
