a56a22d71160d254c0659439d0cd627e304ca676
Alex8608 <aleshka.vlad@gmail.com>
2023-09-27 16:58:33 +0400

    write in file

--- Lab.py
+++ Lab.py
@@ -1,9 +1,13 @@
 import requests
 array=[]
 url="https://www.cbr-xml-daily.ru/archive/2023/09/23/daily_json.js"
-while url!="https://www.cbr-xml-daily.ru/archive/2021/01/01/daily_json.js":
+while url!="https://www.cbr-xml-daily.ru/archive/2023/02/03/daily_json.js":
     response=requests.get(url)
     data=response.json()
     array.append(f'{data["Date"][:10]},{data["Valute"]["USD"]["Value"]}')
     url="https:"+data["PreviousURL"]
 array.reverse()
+file=open("dataset.csv","w")
+s="\n"
+file.write(s.join(array))
+file.close()
