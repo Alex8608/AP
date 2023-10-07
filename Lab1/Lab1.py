5c4e2a9dce49764c2f8f8edb85f22bf5246463b5
Алексей Владимирцев <aleshka.vlad@gmail.com>
2023-10-07 19:11:13 +0400

    Final lab1

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -4,24 +4,16 @@ def date_conrol():
     while True:
         date=str(input())
         try:
-            requests.get(f"https://www.cbr-xml-daily.ru/archive/{date}/daily_json.js").json()["Date"]
+            requests.get(f"https://www.cbr-xml-daily.ru/archive/{date}/daily_json.js", headers={'User-agent':'Mozilla/5.0'}).json()["Date"]
             return f"https://www.cbr-xml-daily.ru/archive/{date}/daily_json.js"
         except:
             print("Курс ЦБ РФ на данную дату не установлен\nУкажите другую дату: ")
-
-print("Введите дату начала периода в формате yyyy/mm/dd: ")
-url1=date_conrol()
 print("Введите дату конца периода в формате yyyy/mm/dd: ")
-url2=date_conrol()
-array=[]
-while url2!=url1:
-    response=requests.get(url2)
+url=date_conrol()
+while url!="https:":
+    response=requests.get(url)
     data=response.json()
-    array.append(f'{data["Date"][:10]},{data["Valute"]["USD"]["Value"]}')
-    url2="https:"+data["PreviousURL"]
-array.append(f'{data["PreviousDate"][:10]},{data["Valute"]["USD"]["Previous"]}')
-array.reverse()
-file=open("./Lab1/dataset.csv","w")
-s="\n"
-file.write(s.join(array))
-file.close()
+    print(data["Date"][:10])
+    url="https:"+data["PreviousURL"]
+    with open("./Lab1/dataset.csv","a") as file:
+        file.write(f'{data["Date"][:10]},{data["Valute"]["USD"]["Value"]}\n')
